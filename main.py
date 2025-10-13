46db4acfcd9ac2fad3c2fc4a57b620204f949c40
pale <brodstar_3000@hotmail.com>
2025-10-13 15:57:29 +1100

    update constants.py

--- main.py
+++ main.py
@@ -1,20 +1,26 @@
 # this allows us to use code from the open-source pygame library throughout this file
 import pygame
 from constants import *
-
+from player import Player
 
 def main():
     pygame.init()
-    print("Starting Asteroids!")
-    print("Screen width:", SCREEN_WIDTH)
-    print("Screen height:", SCREEN_HEIGHT)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
     while True:
-        screen.fill("black")
-        pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        player.update(dt)
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
+
+
 
 if __name__ == "__main__":
     main()
